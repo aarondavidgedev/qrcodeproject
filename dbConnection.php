bcf5d7647d6f043f10fd1a434008409201c57698
aarondavidgedev <aarondavidgeoptimusprime@gmail.com>
2022-04-07 18:47:51 -0500

    db connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$dbServerName = "example.com";
-$dbUsername = "exdbuser";
-$dbPassword = "exdbpass";
-$dbName = "codexworld";
+$dbServerName = "localhost";
+$dbUsername = "root";
+$dbPassword = "password";
+$dbName = "testdb";
 
 // create connection
 $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
@@ -11,5 +11,13 @@ $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "\n";
+echo "You've SUCCESSFULLY connected to your SQL database.\n";
+echo "The current date is: "; echo date('Y/m/d'); echo("\n"); 
+echo "Your connection details are: \n";
+echo "\n";
+echo "Server Name: $dbServerName \n";
+echo "Username: $dbUsername \n";
+echo "Password: $dbpassword \n";
+echo "Database Name: $dbName \n";
 ?>
